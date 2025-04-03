3fb4e81b6065a65b4c48f4d3649ae6c9c6591692
Mohamed Ahmed <MohamedAhmed@MacBooks-MacBook-Air.local>
2025-04-03 21:28:29 +0200

    switch database host to local

--- InsertEmp.py
+++ InsertEmp.py
@@ -5,14 +5,14 @@ def insert_to_table(form_product_id):
     try:
         connection = psycopg2.connect(user="testdb_pc0s_user",
                                     password="CpWMnbHsqZM751EgEjbzHyhGGfXfNmPk",
-                                    host="dpg-cvndib63jp1c738hnrn0-a.oregon-postgres.render.com",
+                                    host="dpg-cvndib63jp1c738hnrn0-a",
                                     port="5432",
                                     database="testdb_pc0s")
         cursor = connection.cursor()
 
         postgres_insert_query = """ INSERT INTO employee (personal_number, name) VALUES (%s,%s)"""
         product_id =  str(form_product_id)
-        record_to_insert = ('emp002', 'Ahmed Mahmoud')
+        record_to_insert = ('emp003', 'Mahmoud Seddik')
         cursor.execute(postgres_insert_query, record_to_insert)
 
         connection.commit()
