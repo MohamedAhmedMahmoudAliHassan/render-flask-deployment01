7af0d85d04194448c8cbff69a4d60f625507b75a
Mohamed Ahmed <MohamedAhmed@MacBooks-MacBook-Air.local>
2025-04-03 21:23:28 +0200

    create hyper link to add in cloud database

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+from InsertEmp import insert_to_table
 
 app = Flask(__name__)
 
+@app.route('/run_script')
+def run_script():
+    insert_to_table()
+
 @app.route("/", methods=['Get'])
 def home():
-    return "Flask App5"
+    return '<a href="/run_script">Add new employee</a>'
 
 if __name__ == "__main__":
     app.run()
