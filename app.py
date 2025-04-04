a3a6e28b94455b5ed3c8c9e8ae3137ffd91fd672
Mohamed Ahmed <MohamedAhmed@MacBooks-MacBook-Air.local>
2025-04-04 13:59:51 +0200

    render base html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 from InsertEmp import insert_to_table
 
 app = Flask(__name__)
@@ -6,11 +6,12 @@ app = Flask(__name__)
 @app.route('/run_script')
 def run_script():
     insert_to_table()
-    return 'Employee added successfully'
+    return render_template('base.html')
 
 @app.route("/", methods=['Get'])
 def home():
-    return '<a href="/run_script">Add new employee</a>'
+    #return '<a href="/run_script">Add new employee</a>'
+    return render_template('index.html') 
 
 if __name__ == "__main__":
     app.run()
