f3a901e41c6f0e3a19e11b2ea063f014ab259878
Mohamed Ahmed <MohamedAhmed@MacBooks-MacBook-Air.local>
2025-04-05 14:04:19 +0200

    switch database connection to cloud

--- InsertEmp.py
+++ InsertEmp.py
@@ -3,22 +3,24 @@ import psycopg2
 
 def insert_to_table(personal_number, employee_name):
     try:
-        '''
+
         #Internal database host from cloud to cloud 
         connection = psycopg2.connect(user="testdb_pc0s_user",
                             password="CpWMnbHsqZM751EgEjbzHyhGGfXfNmPk",
                             host="dpg-cvndib63jp1c738hnrn0-a",
                             port="5432",
                             database="testdb_pc0s")
-        '''
+ 
 
-    
+        '''
         # Internal database connection from local to local
         connection = psycopg2.connect(user="postgres",
                                     password="postgres",
                                     host="127.0.0.1",
                                     port="5432",
                                     database="testdb")
+        '''
+
 
         
         cursor = connection.cursor()
--- SelectEmp.py
+++ SelectEmp.py
@@ -3,21 +3,24 @@ import psycopg2
 
 def select_emp():
     try:
-        '''
+
         #Internal database host from cloud to cloud 
         connection = psycopg2.connect(user="testdb_pc0s_user",
                             password="CpWMnbHsqZM751EgEjbzHyhGGfXfNmPk",
                             host="dpg-cvndib63jp1c738hnrn0-a",
                             port="5432",
                             database="testdb_pc0s")
-        '''
+
         
+        '''
         # Internal database connection from local to local
         connection = psycopg2.connect(user="postgres",
                                     password="postgres",
                                     host="127.0.0.1",
                                     port="5432",
                                     database="testdb")
+        '''
+
         
         
         cur = connection.cursor()
